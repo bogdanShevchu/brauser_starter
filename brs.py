28a1b6d3b40bb72b918e1b0aa6f64fd69cceab68
bogdanShevchu <iophj4845@gmail.com>
2026-02-16 18:21:28 +0100

    version_1.0

--- brs.py
+++ brs.py
@@ -3,8 +3,8 @@ import webbrowser as wb
 def open_url(url):
 	wb.open(url)
 
-print("1 = start_1_linc, 1p = save_1_linc:")
 while True:
+	print("2 = start_2_linc, 2p = save_2_linc:")
 	vi = input("1 = start_1_linc, 1p = save_1_linc:")
 	if vi == "1p":
 		linc1 = open("linc1", "w")
@@ -16,5 +16,19 @@ while True:
 		linc1 = open("linc1", "r")
 		linc_1_st = linc1.read()
 		open_url(linc_1_st)
+		linc1.close()
+		vi = None
+		break
+	elif vi == "2p":
+		linc2 = open("linc2", "w")
+		l2 = input("write linc")
+		linc2.write(l2)
+		linc2.close()
+		vi = None
+	elif vi == "2":
+		linc2 = open("linc2", "r")
+		linc_2_st = linc2.read()
+		open_url(linc_2_st)
+		linc2.close()
 		vi = None
 		break
